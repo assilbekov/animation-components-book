326905efd8a963e28e4cfbea7046f37073839bc9
assilbekov <askhat.assilbekov@gmail.com>
2025-09-30 00:46:50 +0500

    add SpaceShipSvg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { ScrollingHeadphones } from "@/components/scrolling-headphones";
 import { SvgLightOnHover } from "@/components/svg-light-on-hover";
 import { SvgMorphingTheme } from "@/components/svg-morphing-theme";
+import { SpaceShipSvg } from "@/components/space-ship";
 
 export default function Home() {
   return (
     <div>
+      <SpaceShipSvg />
       <SvgMorphingTheme />
       <SvgLightOnHover />
       <ScrollingHeadphones />
